affd53c2cd1f6ed274f16b122f9f5a32a05ccccb
shakeem-brown <shakeem.brown@mymail.champlain.edu>
2020-02-21 16:54:04 -0500

    Started the framework development for project setup.

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.cpp
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.cpp
@@ -1,7 +1,11 @@
 // GPRO-FW-Launcher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <stdio.h>
+
 int main()
 {
+	int test = foo(9000);
 
+	print("\n %d \n", test);
 }
